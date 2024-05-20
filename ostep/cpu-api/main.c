79ab75323a7de3b019e93210b919ce518b92c992
Ramana Venkata <vramana@users.noreply.github.com>
2024-05-20 22:46:48 +0530

    Add ostep chapter 5 homework

---
+++ ostep/cpu-api/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+  printf("Hello (pid:%d)\n", (int) getpid());
+  int rc = fork();
+  if (rc < 0) {
+    fprintf(stderr, "fork failed\n");
+    exit(1);
+  } else if (rc == 0) {
+    printf("Hello, I am child (pid:%d)\n", (int) getpid());
+  } else {
+    printf("Hello, I am parent of %d (pid:%d)\n", rc, (int) getpid());
+  }
+}
---
+++ ostep/cpu-api/src/work-1.zig
@@ -0,0 +1,11 @@
+const c = @cImport(
+    @cInclude("stdlib.h"),
+    @cInclude("unistd.h"),
+);
+const std = @import("std");
+
+pub fn main() !void {
+
+
+
+}
