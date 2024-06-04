ab7528140d94a943e59cffcaa6028e437341bae5
Ramana Venkata <vramana@users.noreply.github.com>
2024-06-04 14:48:50 +0530

    Simple SDL example

---
+++ visualize/token-visualizer/README.md
@@ -0,0 +1,19 @@
+# SDL2 Zig Demo
+
+Here's a basic window with SDL2 in Zig.
+
+![screenshot](screenshot1.png)
+
+## How to build and run it
+
+```
+zig build run
+```
+
+## Cross-Compiling
+
+Pass a `-Dtarget` option. For example:
+
+```
+zig build -Dtarget=x86_64-windows
+```
---
+++ visualize/token-visualizer/build.zig
@@ -0,0 +1,22 @@
+const Build = @import("std").Build;
+
+pub fn build(b: *Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "sdl-zig-demo",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.linkSystemLibrary("SDL2");
+    exe.linkLibC();
+
+    b.installArtifact(exe);
+
+    const run = b.step("run", "Run the demo");
+    const run_cmd = b.addRunArtifact(exe);
+    run.dependOn(&run_cmd.step);
+}
