66a380201c511af73a85f50ced0a068824a0fa37
Ramana Venkata <vramana@users.noreply.github.com>
2024-11-24 22:51:54 +0530

    Commit unfinished changes

--- chapter-08/src/main.zig
+++ chapter-08/src/main.zig
@@ -3,18 +3,47 @@ const Parser = @import("parser.zig").Parser;
 const AstType = @import("parser.zig").AstType;
 const Ast = @import("parser.zig").Ast;
 
-fn asm_statement(allocator: std.mem.Allocator, list: *std.ArrayList(u8), node: *const Ast) !void {
-    _ = allocator;
+fn asm_number(list: *std.ArrayList(u8), node: *const Ast) !void {
+    switch (node.*) {
+        AstType.Number => |n| {
+            try list.appendSlice("\tmov x0, #");
+            try list.appendSlice(n);
+            try list.append('\n');
+        },
+        else => unreachable,
+    }
+}
+
+fn asm_add(list: *std.ArrayList(u8), node: *const Ast) !void {
+    switch (node.*) {
+        AstType.BinaryOperator => |value| {
+            try asm_expression(list, value.left);
+            try list.appendSlice("\tmov x1, x0\n");
+            try asm_expression(list, value.right);
+            try list.appendSlice("\tadd x0, x0, x1\n");
+        },
+        else => unreachable,
+    }
+}
+
+fn asm_expression(list: *std.ArrayList(u8), node: *const Ast) anyerror!void {
+    switch (node.*) {
+        AstType.BinaryOperator => |value| {
+            if (std.mem.eql(u8, value.op, "+")) {
+                try asm_add(list, node);
+            }
+        },
+        AstType.Number => |_| {
+            try asm_number(list, node);
+        },
+        else => unreachable,
+    }
+}
+
+fn asm_statement(list: *std.ArrayList(u8), node: *const Ast) !void {
     switch (node.*) {
         AstType.Var => |value| {
-            switch (value.value.*) {
-                AstType.Number => |n| {
-                    try list.appendSlice("\tmov x0, #");
-                    try list.appendSlice(n);
-                    try list.append('\n');
-                },
-                else => unreachable,
-            }
+            try asm_expression(list, value.value);
         },
         else => unreachable,
     }
@@ -33,7 +62,7 @@ fn assembler(allocator: std.mem.Allocator, parser: *Parser) !void {
             for (value.items) |_node| {
                 const t: AstType = _node;
                 if (t == AstType.Var) {
-                    try asm_statement(allocator, &list, &_node);
+                    try asm_statement(&list, &_node);
                 }
             }
         },
@@ -58,7 +87,7 @@ pub fn main() !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    const string_input = "var a = 3; var b = 5; var c = 10;";
+    const string_input = "var a = 3; var b = 5; var c = 10 + 23;";
 
     var parser = try Parser.init(allocator, string_input);
 
