c94c4fedb7edc0fc659d9577318c65bac60aef80
Ramana Venkata <vramana@users.noreply.github.com>
2024-12-19 11:47:38 +0530

    Add rust-os chapter-1

---
+++ rust-os/README.md
@@ -0,0 +1,10 @@
+# Minimal Rust OS
+
+From the blog post series [Writing an OS in Rust](https://os.phil-opp.com/).
+
+## Building
+
+```sh
+cargo build
+```
+
---
+++ rust-os/src/main.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let _a = 0;
+    loop {}
+}
